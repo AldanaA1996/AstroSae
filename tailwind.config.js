be0de8fa2750e6dd47248a4f7a689fde2fe1e830
AldanaA1996 <albani.aldana@gmail.com>
2025-02-04 22:53:59 -0300

    fix up

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require('tailwindcss/defaultTheme');
-
-export default {
-  content: ["./src/**/*.{astro,html,jsx,tsx,svelte,vue,js,ts}"],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ["Poppins-Regular", ...defaultTheme.fontFamily.sans],
-      },
-      colors: {
-        verdekaki: '#D6D58E',
-        lima: '#DBF227',
-        verde: '#9FC131',
-        verdeoscuro: '#005c53',
-        verdeazulado: '#042940',
-      },
-      screens: {
-        sm: "400px",
-      },
-    },
-  },
-  plugins: [],
-}
+module.exports = {
+	content: ["./src/**/*.{astro,html,jsx,tsx,svelte,vue,js,ts}"],
+	theme: {
+		extend: {
+			screens: {
+				sm: "400px",
+			},
+		},
+	},
+	plugins: [],
+};
